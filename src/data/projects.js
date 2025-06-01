4ffdf36748a92c07f42c9a11f2b450b60de868e8
ozi-Shalom2333 <okaozisol45@gmail.com>
2025-06-01 13:08:46 +0100

    portfolio update

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,6 +17,24 @@ const projects = [
     githubLink: 'https://github.com/ozi-Shalom2333/TodolistdragAnd-Drop',
     tech: ['React', 'CSS'],
   },
+  {
+    id: 3,
+    title: 'canny.io replica',
+    description: 'My attempt at replicating canny.io landing page',
+    image: '/assets/cannyimg.png',
+    liveLink: 'https://canny-ten.vercel.app/',
+    githubLink: 'https://github.com/ozi-Shalom2333/canny',
+    tech: ['Html', 'CSS', 'Javascript'],
+  },
+  {
+    id: 4,
+    title: 'Voting App',
+    description: 'A user-friendly voting app enabling secure, transparent elections with real-time results and seamless mobile access for all.',
+    image: '/assets/voters.jpg',
+    liveLink: 'https://oziomavote-app.vercel.app/',
+    githubLink: 'https://github.com/ozi-Shalom2333/voteApp',
+    tech: ['Html', 'CSS', 'React'],
+  },
 ];
 
 export default projects;
--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -1,11 +1,11 @@
 export const theme = {
     colors: {
-      primary: '#1C2526', 
-      secondary: '#3C4647', 
-      background: '#E8ECEF', 
-      text: '#F8F9FA', 
-      textDark: '#212529', 
-      accent: '#6B7280', 
+      primary: ' #1C2526', 
+      secondary: ' #3C4647', 
+      background: ' #E8ECEF', 
+      text: ' #F8F9FA', 
+      textDark: ' #212529', 
+      accent: ' #6B7280', 
     },
     fonts: {
       main: "'Roboto', sans-serif",
